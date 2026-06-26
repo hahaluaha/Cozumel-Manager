77076c0787c10f28266698c5e7a56cb22bd34898
fernandogonzalez <33910020+hahaluaha@users.noreply.github.com>
2026-06-26 17:57:34 -0500

    feat: wire Sparkle 2 updater and add Check for Updates menu item

--- CozumelManager/CozumelManager/CozumelManagerApp.swift
+++ CozumelManager/CozumelManager/CozumelManagerApp.swift
@@ -1,13 +1,26 @@
 import SwiftUI
+import Sparkle
 
 @main
 struct CozumelManagerApp: App {
     @StateObject private var store = PropertyStore()
+    private let updaterController = SPUStandardUpdaterController(
+        startingUpdater: true,
+        updaterDelegate: nil,
+        userDriverDelegate: nil
+    )
 
     var body: some Scene {
         Window("Cozumel Manager", id: "main") {
             MainDashboardView()
                 .environmentObject(store)
+        }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                Button("Check for Updates…") {
+                    updaterController.checkForUpdates(nil)
+                }
+            }
         }
     }
 }
