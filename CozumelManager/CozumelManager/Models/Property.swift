f2c4b2b4f9ef636796d32cb39e93e268d58b117a
fernandogonzalez <33910020+hahaluaha@users.noreply.github.com>
2026-07-03 16:57:38 -0500

    feat: add guest-tiered pricing fields and nightlyRate(forGuests:) to Property

--- CozumelManager/CozumelManager/Models/Property.swift
+++ CozumelManager/CozumelManager/Models/Property.swift
@@ -25,12 +25,17 @@ struct Property: Identifiable, Hashable, Codable {
     var address: String
     var baseRate: Double
     var monthlyPrice: Double?
+    var baseGuests: Int?
+    var maxGuests: Int?
+    var extraGuestFee: Double?
     var status: PropertyStatus
     var unavailableDateRanges: [DateRange]
     var photos: [URL]
 
     init(id: String, name: String, neighborhood: String, address: String,
-         baseRate: Double, monthlyPrice: Double? = nil, status: PropertyStatus,
+         baseRate: Double, monthlyPrice: Double? = nil,
+         baseGuests: Int? = nil, maxGuests: Int? = nil, extraGuestFee: Double? = nil,
+         status: PropertyStatus,
          unavailableDateRanges: [DateRange] = [], photos: [URL] = []) {
         self.id = id
         self.name = name
@@ -38,6 +43,9 @@ struct Property: Identifiable, Hashable, Codable {
         self.address = address
         self.baseRate = baseRate
         self.monthlyPrice = monthlyPrice
+        self.baseGuests = baseGuests
+        self.maxGuests = maxGuests
+        self.extraGuestFee = extraGuestFee
         self.status = status
         self.unavailableDateRanges = unavailableDateRanges
         self.photos = photos
@@ -48,6 +56,13 @@ struct Property: Identifiable, Hashable, Codable {
         return monthlyPrice ?? baseRate * 22
     }
 
+    func nightlyRate(forGuests guests: Int) -> Double {
+        guard let baseGuests, let extraGuestFee, guests > baseGuests else { return baseRate }
+        let cappedGuests = min(guests, maxGuests ?? guests)
+        let extra = max(0, cappedGuests - baseGuests)
+        return baseRate + Double(extra) * extraGuestFee
+    }
+
     static func == (lhs: Property, rhs: Property) -> Bool { lhs.id == rhs.id }
     func hash(into hasher: inout Hasher) { hasher.combine(id) }
 
@@ -55,6 +70,9 @@ struct Property: Identifiable, Hashable, Codable {
         case id, name, neighborhood, address, status, photos
         case baseRate = "base_rate"
         case monthlyPrice = "monthly_price"
+        case baseGuests = "base_guests"
+        case maxGuests = "max_guests"
+        case extraGuestFee = "extra_guest_fee"
         case unavailableDateRanges = "unavailable_date_ranges"
     }
 
@@ -66,6 +84,9 @@ struct Property: Identifiable, Hashable, Codable {
         address = try c.decode(String.self, forKey: .address)
         baseRate = try c.decode(Double.self, forKey: .baseRate)
         monthlyPrice = try? c.decode(Double.self, forKey: .monthlyPrice)
+        baseGuests = try? c.decode(Int.self, forKey: .baseGuests)
+        maxGuests = try? c.decode(Int.self, forKey: .maxGuests)
+        extraGuestFee = try? c.decode(Double.self, forKey: .extraGuestFee)
         status = try c.decode(PropertyStatus.self, forKey: .status)
         unavailableDateRanges = (try? c.decode([DateRange].self, forKey: .unavailableDateRanges)) ?? []
         photos = (try? c.decode([URL].self, forKey: .photos)) ?? []
